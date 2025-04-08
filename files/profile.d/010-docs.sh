4f9339dbd24de4cc68196163f1a6a1b0d4bb12c8
François KUBLER <francois+github@kubler.org>
2025-04-08 14:54:16 +0200

    fix: set env at runtime

--- files/profile.d/010-docs.sh
+++ files/profile.d/010-docs.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
 
-PATH="${PATH}:${HOME}/src/frontend/.scalingo/node/bin:${HOME}/src/frontend/.scalingo/yarn/bin:${HOME}/src/frontend/node_modules"
+PATH="${PATH}:${HOME}/src/frontend/.scalingo/node/bin"
+PATH="${PATH}:${HOME}/src/frontend/.scalingo/yarn/bin"
+PATH="${PATH}:${HOME}/src/frontend/node_modules"
+
+PATH="${PATH}:${HOME}/src/backend/.scalingo/python/bin/"
 export PATH
+
+PYTHONUNBUFFERED="true"
+export PYTHONUNBUFFERED
+
+PYTHONHOME="${HOME}/src/backend/.scalingo/python"
+export PYTHONHOME
+
+LIBRARY_PATH="${LIBRARY_PATH:+:${LIBRARY_PATH}}:${HOME}/src/backend/.scalingo/python/lib"
+export LIBRARY_PATH
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}:${HOME}/src/backend/.scalingo/python/lib"
+export LD_LIBRARY_PATH
